5de6de0626f56aba39041271bc7b847974933e02
Ruan Silva <ks9957591@gmail.com>
2026-03-28 14:43:57 -0300

    add de inserts

---
+++ tipos de insert.sql
@@ -0,0 +1,8 @@
+USE db_locadora;
+
+-- INSERT unico 
+-- INSERT INTO tb_categoria (descricao) VALUES ('Jogos');
+
+
+-- INSERT aninhado
+-- INSERT INTO tb_categoria (descricao) VALUES ('RPG'),('RPG 1');
