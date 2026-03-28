290016fc9073b1cbaa0d6a37a5498ea95387152e
Ruan Silva <ruanzsilva23@gmail.com>
2026-03-28 15:05:25 -0300

    Delete add inserts.sql

--- add inserts.sql
+++
@@ -1,9 +0,0 @@
-USE db_locadora;
-
--- INSERT unico 
--- INSERT INTO tb_categoria (descricao) VALUES ('Jogos');
-
-
--- INSERT aninhado
--- INSERT INTO tb_categoria (descricao) VALUES ('RPG'),('RPG 1');
--- diferente
