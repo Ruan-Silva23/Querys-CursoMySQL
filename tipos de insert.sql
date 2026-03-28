c05faeffeb2cbda81df8c943f578ee3c8d7a51cb
Ruan Silva <ruanzsilva23@gmail.com>
2026-03-28 14:55:34 -0300

    Delete tipos de insert.sql

--- tipos de insert.sql
+++
@@ -1,8 +0,0 @@
-USE db_locadora;
-
--- INSERT unico 
--- INSERT INTO tb_categoria (descricao) VALUES ('Jogos');
-
-
--- INSERT aninhado
--- INSERT INTO tb_categoria (descricao) VALUES ('RPG'),('RPG 1');
